05cfc188f237fc9fa47e85a83e3761fa1a3fe582
cams-z <cameliaelmayah@gmail.com>
2025-12-14 23:32:50 +0100

    Add files via upload
    Flask app qui connecte l'interface HTML au jeu du Simon

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, send_file
+import threading
+from squelette_code_python import game
+import logging
+
+app = Flask(__name__)
+
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
+
+game_status = "En attente"
+
+@app.route("/")
+def index():
+    return send_file("site_projo.html")
+
+@app.route("/start")
+def start():
+    global game_status
+    game_status = "Jeu en cours"
+    threading.Thread(target=game).start()
+    return "ok"
+
+@app.route("/status")
+def status():
+    return game_status
+
+@app.route("/css-site.css")
+def css():
+    return send_file("css-site.css")
+
+@app.route("/pierre-augustine.gif")
+def img():
+    return send_file("pierre-augustine.gif")
+
+if __name__ == "__main__":
+    app.run(debug=True)
