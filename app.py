2b2c1033953768a7bd7029b980d9089e6d42085c
cams-z <cameliaelmayah@gmail.com>
2025-12-15 22:54:47 +0100

    lien entre python et html pour le score en live

--- app.py
+++ app.py
@@ -6,12 +6,9 @@ import logging
 app = Flask(__name__)
 logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
-# Statut du jeu
 game_status = "Lancer le jeu ?"
-# Score final
+current_score = 0
 final_score = 0
-# é
-# eviter de lancer plusieur parties en meme temps
 game_thread = None
 
 @app.route("/")
@@ -19,16 +16,20 @@ def index():
     return send_file("site_projo.html")
 
 def run_game():
-    """Exécute le jeu et met à jour le statut et le score final"""
-    global game_status, final_score
-    game_status = "Jeu en cours"
-    score = game()
-    final_score = score
-    game_status = f"PERDU ! — score : {score}"
+    global game_status, current_score, final_score
+
+    current_score = 0
+    game_status = "Jeu lancer !"
+
+    def update_score(score):
+        global current_score
+        current_score = score   # score mis à jour en live
+
+    final_score = game(update_score)
+    game_status = f"PERDU !"
 
 @app.route("/start")
 def start():
-    """Lancer le jeu via un thread pour ne pas bloquer Flask"""
     global game_thread
     if game_thread is None or not game_thread.is_alive():
         game_thread = threading.Thread(target=run_game)
@@ -37,24 +38,22 @@ def start():
 
 @app.route("/status")
 def status():
-    """Renvoie le statut actuel du jeu"""
     return game_status
 
 @app.route("/score")
-def score_route():
-    """Renvoie le score final"""
-    return str(final_score)
+def score():
+    return str(current_score)
 
 @app.route("/css-site.css")
 def css():
     return send_file("css-site.css")
 
 @app.route("/pierre-augustine.gif")
-def pigeon_gif():
+def pigeon():
     return send_file("pierre-augustine.gif")
 
 @app.route("/simon.gif")
-def simon_gif():
+def simon():
     return send_file("simon.gif")
 
 if __name__ == "__main__":
