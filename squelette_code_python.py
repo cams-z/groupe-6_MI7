9eb9fe62fe6d2fbb072c240414ad716ec26fd1d3
cams-z <cameliaelmayah@gmail.com>
2025-12-15 22:52:50 +0100

    modification pour récupérer le score a chaque tours

--- squelette_code_python.py
+++ squelette_code_python.py
@@ -13,6 +13,7 @@ def game_round(s):
     for e1 in s:
         print(e1)
         time.sleep(1)
+
     print("à vous de jouer")
     for e2 in s:
         sequence_input = int(input())
@@ -20,14 +21,14 @@ def game_round(s):
             return False
     return True
 
-def game():
+def game(update_score_callback):
     score = 0
     s = initialise_sequence()
 
     while game_round(s):
         s = add_to_sequence(s)
         score += 50
+        update_score_callback(score)   # renvoie score en temps reel
 
     print("perdu")
-    print("score final :", score)
     return score
